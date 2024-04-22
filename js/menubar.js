3dff1223b5001c00757a0fd9d4e44846288266bd
Effyiex <leakforumse@gmail.com>
2024-04-22 15:21:56 +0200

    Fold Menu Bodies (Menubar)

--- js/menubar.js
+++ js/menubar.js
@@ -1,11 +1,55 @@
 
+const LIGHTMODE = {
+    localStoreKey: "effyiex.lightmode",
+    get active() {
+        return (localStorage.getItem(this.localStoreKey) == "true");
+    },
+    set active(value) {
+        localStorage.setItem(this.localStoreKey, value.toString());
+        this.updateDOM();
+    },
+    updateDOM: function() {
+        if(this.active) {
+            if(!document.body.classList.contains("lightmode"))
+            document.body.classList.add("lightmode");
+        } else {
+            if(document.body.classList.contains("lightmode"))
+            document.body.classList.remove("lightmode");
+        }
+    },
+    toggle: function() {
+        this.active = !this.active
+    }
+};
+
 window.addEventListener("load", () => {
+
     const menubarElem = document.querySelector(".menubar");
     const foldMenuHeadElems = menubarElem.querySelectorAll("h3");
+    const foldMenuBodyElems = menubarElem.querySelectorAll(".fold-menus div");
     const controlsElem = menubarElem.querySelector(".controls");
     const lightSwitchElem = controlsElem.querySelector(".lightswitch");
-    lightSwitchElem.addEventListener(
-        "click", 
-        () => document.body.classList.toggle("lightmode")
-    );
+
+    LIGHTMODE.updateDOM();
+    lightSwitchElem.addEventListener("click", LIGHTMODE.toggle.bind(LIGHTMODE));
+
+    foldMenuHeadElems.forEach((el, i) => {
+
+        function show() {
+            if(foldMenuBodyElems[i] && !foldMenuBodyElems[i].classList.contains("shown"))
+            foldMenuBodyElems[i].classList.add("shown");
+        }
+
+        function hide() {
+            if(foldMenuBodyElems[i] && foldMenuBodyElems[i].classList.contains("shown"))
+            foldMenuBodyElems[i].classList.remove("shown");
+        }
+
+        el.addEventListener("focus", show);
+        el.addEventListener("blurr", hide);
+        el.addEventListener("mouseover", show);
+        el.addEventListener("mouseleave", hide);
+
+    });
+
 });
--- js/wallpaper.js
+++ js/wallpaper.js
@@ -14,7 +14,7 @@ const WALLPAPER = {
     ctx.fillStyle = "#131112";
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
     if(
-      this.frameCount % 32 == 0
+      this.frameCount % 64 == 0
       || this.mountains.length == 0
     ) {
       const heights = [];
@@ -35,7 +35,7 @@ const WALLPAPER = {
         + this.stars[i].scale * 32
         + (this.stars[i].initX > 0.5 ? (1 - this.stars[i].x) : (this.stars[i].x)) * 160
       ).toString(16).padStart(2, "0");
-      ctx.fillRect(this.stars[i].x  * this.canvas.width, this.stars[i].y * this.canvas.height / 3 * 2, this.stars[i].scale * this.canvas.width * 0.01, this.stars[i].scale * this.canvas.height * 0.01);
+      ctx.fillRect(this.stars[i].x  * this.canvas.width, this.stars[i].y * this.canvas.height / 4 * 3, this.stars[i].scale * this.canvas.width * 0.01, this.stars[i].scale * this.canvas.height * 0.01);
       this.stars[i].pulse += 0.1 * this.stars[i].pulseFactor;
       if(
         this.stars[i].pulse > 1
@@ -43,15 +43,15 @@ const WALLPAPER = {
       ) {
         this.stars[i].pulseFactor *= -1;
       }
-      this.stars[i].y += this.stars[i].scale * 0.001;
-      this.stars[i].x -= (0.5 - this.stars[i].initX) * 0.004;
+      this.stars[i].y += this.stars[i].scale * 0.0025;
+      this.stars[i].x -= (0.5 - this.stars[i].initX) * 0.01;
       if(
         this.stars[i].x > 1 
         || this.stars[i].x < 0 
         || this.stars[i].y > 1
       ) {
-        this.stars[i].initX = Math.random();
-        this.stars[i].initY = Math.random();
+        this.stars[i].initX = (i % 2 == 0 ? 0 : 0.5) + Math.random() * 0.5;
+        this.stars[i].initY = Math.random() * 0.5;
         this.stars[i].x = this.stars[i].initX;
         this.stars[i].y = this.stars[i].initY;
       }
@@ -63,8 +63,8 @@ const WALLPAPER = {
         continue;
       }
       this.mountains[i].yLevel += 0.002;
-      ctx.fillStyle = "#9933CC" + Math.floor(this.mountains[i].yLevel * 48).toString(16).padStart(2, "0");
-      let translateY = this.mountains[i].yLevel * this.canvas.height;
+      ctx.fillStyle = "#9933CC" + Math.floor(this.mountains[i].yLevel * 80).toString(16).padStart(2, "0");
+      let translateY = this.mountains[i].yLevel * this.canvas.height / 2;
       ctx.beginPath();
       ctx.moveTo(-this.canvas.width / 4, this.canvas.height / 2 + translateY);
       for(let j = 0; j < this.mountains[i].heights.length; j++) {
